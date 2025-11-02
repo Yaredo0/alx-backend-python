070fa223082af76fb99263ce5b87efd5382cb317
Yaredo0 <yared4sy@gmail.com>
2025-11-02 19:42:05 +0300

    2

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -2,30 +2,26 @@ import psycopg2
 import psycopg2.extras
 
 def stream_users_in_batches(batch_size):
-    try:
-        connection = psycopg2.connect(
-            dbname="alx_prodev",
-            user="postgres",
-            password="",
-            host="localhost"
-        )
-        cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        cursor.execute("SELECT * FROM user_data")
+    connection = psycopg2.connect(
+        dbname="alx_prodev",
+        user="postgres",
+        password="yared@pgdb",
+        host="localhost"
+    )
+    cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor.execute("SELECT * FROM user_data")
 
-        while True:
-            batch = cursor.fetchmany(batch_size)
-            if not batch:
-                break
-            yield [dict(row) for row in batch]
+    while True:
+        rows = cursor.fetchmany(batch_size)
+        if not rows:
+            break
+        for row in rows:
+            yield dict(row)
 
-        cursor.close()
-        connection.close()
-
-    except Exception as e:
-        print("Error streaming batches:", e)
+    cursor.close()
+    connection.close()
 
 def batch_processing(batch_size):
-    for batch in stream_users_in_batches(batch_size):
-        for user in batch:
-            if user['age'] > 25:
-                yield user
+    for user in stream_users_in_batches(batch_size):
+        if user["age"] > 25:
+            yield user
